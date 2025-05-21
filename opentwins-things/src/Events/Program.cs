a7b46d09a63422a0f8906c7f43872efed274988c
Julia Robles <48439828+juliarobles@users.noreply.github.com>
2025-05-21 11:47:03 +0200

    now subscribes to events

--- opentwins-things/src/Events/Program.cs
+++ opentwins-things/src/Events/Program.cs
@@ -1,7 +1,37 @@
-using Events;
+// Execute with: dapr run --app-id myapp --app-port 5000 --resources-path ./Components -- dotnet run
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+using Dapr.Messaging.PublishSubscribe;
+using Dapr.Messaging.PublishSubscribe.Extensions;
+using System.Text;
 
-var host = builder.Build();
-host.Run();
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDaprPubSubClient();
+var app = builder.Build();
+
+var messagingClient = app.Services.GetRequiredService<DaprPublishSubscribeClient>();
+
+Console.WriteLine("Me subscribo");
+//Create a dynamic streaming subscription
+var subscription = await messagingClient.SubscribeAsync("mypubsub", "opentwinsv2-events",
+    new DaprSubscriptionOptions(new MessageHandlingPolicy(TimeSpan.FromSeconds(10), TopicResponseAction.Retry)),
+    HandleMessageAsync, CancellationToken.None);
+
+
+// Test with: dapr publish --pubsub mypubsub --topic opentwinsv2-events --data '{ "type": "event", "value": 42 }' --publish-app-id myapp
+Task<TopicResponseAction> HandleMessageAsync(TopicMessage message, CancellationToken cancellationToken = default)
+{
+    try
+    {
+        //Do something with the message
+        Console.WriteLine("Ha llegado algo mi reina");
+        Console.WriteLine(Encoding.UTF8.GetString(message.Data.Span));
+        //Console.WriteLine(Encoding.UTF8.GetString(message.Data.Span));
+        return Task.FromResult(TopicResponseAction.Success);
+    }
+    catch
+    {
+        return Task.FromResult(TopicResponseAction.Drop);
+    }
+}
+
+await app.RunAsync();
